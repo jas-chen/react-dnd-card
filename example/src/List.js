57e484f7f8e94fcc48172fa9db55281b15fd4b03
Jas Chen <jas228@gmail.com>
2016-05-25 17:02:17 +0800

    Update example to use v0.3.7

--- example/src/List.js
+++ example/src/List.js
@@ -5,9 +5,14 @@ import HTML5Backend from 'react-dnd-html5-backend';
 import DndCard from 'react-dnd-card';
 import { Item, createItem } from './Item';
 
+// set to `true` to use requestAnimationFrame to handle 1000 items.
+const ID_MODE = true;
+
 function genItems() {
   const items = [];
-  for (let i = 0; i < 500; i++) {
+  const total = ID_MODE ? 1000 : 500;
+
+  for (let i = 0; i < total; i++) {
     const item = { id: i, text: `Item ${i}` };
     items.push(item);
   }
@@ -21,10 +26,12 @@ class List extends Component {
 
     this.state = { items: genItems() };
     this.getItem = this.getItem.bind(this);
-    this.moveCard = this.moveCard.bind(this);
+    this.moveCardByIndex = this.moveCardByIndex.bind(this);
+    this.moveCardById = this.moveCardById.bind(this);
+    this.drawFrame = this.drawFrame.bind(this);
   }
 
-  moveCard(dragIndex, hoverIndex) {
+  moveCardByIndex(dragIndex, hoverIndex) {
     const { items } = this.state;
     const dragItem = items[dragIndex];
 
@@ -36,6 +43,37 @@ class List extends Component {
         ]
       }
     }));
+  }
+
+  moveCardById(dragId, hoverIndex) {
+    const { items } = this.state;
+    const dragItem = items.find(item => item.id === dragId);
+    const dragIndex = items.indexOf(dragItem);
+
+    this.scheduleUpdate({
+      items: {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, dragItem]
+        ]
+      }
+    });
+  }
+
+  scheduleUpdate(updateFn) {
+    this.pendingUpdateFn = updateFn;
+
+    if (!this.requestedFrame) {
+      this.requestedFrame = requestAnimationFrame(this.drawFrame);
+    }
+  }
+
+  drawFrame() {
+    const nextState = update(this.state, this.pendingUpdateFn);
+    this.setState(nextState);
+
+    this.pendingUpdateFn = null;
+    this.requestedFrame = null;
   }
 
   getItem(index) {
@@ -64,10 +102,11 @@ class List extends Component {
         {items.map((item, index) => (
           <DndCard
             key={item.id}
+            id={ID_MODE ? item.id : undefined}
             index={index}
             source={this.getItem}
             createItem={createItem}
-            moveCard={this.moveCard}
+            moveCard={ID_MODE ? this.moveCardById : this.moveCardByIndex}
             noDropOutside={true}
             endDrag={this.endDrag}
             style={this.dragStyle}
